2ccdea9348a871e582b14fd16e867f03e0ba2d1f
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2016-09-28 08:24:40 +0200

    Memory Constructor corrected and import cleaned

--- src/test/brainfuck/virtualmachine/MemoryTest.java
+++ src/test/brainfuck/virtualmachine/MemoryTest.java
@@ -2,7 +2,6 @@ package test.brainfuck.virtualmachine;
 
 import brainfuck.virtualmachine.Memory;
 
-//import junit.framework.TestCase;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -10,6 +9,6 @@ public class MemoryTest{
 	@Test
 	public void memoryConstructor(){
 		Memory mem = new Memory();
-		assertNotNull(true);
+		assertNotNull(mem);
 	}
 }
