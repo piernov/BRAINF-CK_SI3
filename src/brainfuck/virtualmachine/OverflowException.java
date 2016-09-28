157ddbe86c5c155ac0bc85b398b018b7570a5ebc
Nassim Bounouas <NassimBounouas@users.noreply.github.com>
2016-09-28 14:53:34 +0200

    MemoryTest in progress& Look for the better Inheritance for OverflowException

--- src/brainfuck/virtualmachine/OverflowException.java
+++ src/brainfuck/virtualmachine/OverflowException.java
@@ -1,7 +1,19 @@
 package brainfuck.virtualmachine;
 
-public class OverflowException extends Exception {
+public class OverflowException extends RuntimeException {
 	public OverflowException() {
 		super();
+	}
+
+	public OverflowException(String s){
+		super(s);
+	}
+
+	public OverflowException(String s, Throwable throwable){
+		super(s, throwable);
+	}
+
+	public OverflowException(Throwable throwable){
+		super(throwable);
 	}
 }
--- src/test/brainfuck/virtualmachine/MemoryTest.java
+++ src/test/brainfuck/virtualmachine/MemoryTest.java
@@ -1,11 +1,20 @@
 package test.brainfuck.virtualmachine;
 
 import brainfuck.virtualmachine.Memory;
+import brainfuck.virtualmachine.OverflowException;
+
+import java.lang.reflect.Method;
+import java.lang.Class;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
 
 public class MemoryTest{
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+
 	@Test
 	public void memoryConstructorAllocation(){
 		Memory mem = new Memory();
@@ -16,5 +25,34 @@ public class MemoryTest{
 	public void memoryConstructorDefaultSize(){
 		Memory mem = new Memory();
 		assertEquals(30000, mem.getSize());
+	}
+
+	@Test
+	public void memoryConstructorSize(){
+		Memory mem = new Memory(21432);
+		assertEquals(21432, mem.getSize());
+	}
+/*
+	@Test//(expected = OverflowException.class)
+	public void checkBoundsUnderflow(){
+		Memory mem = new Memory();
+		thrown.except((Throwable)OverflowException.class);
+		try{
+			Method method = Memory.class.getDeclaredMethod("checkBounds",Integer.TYPE);
+			method.setAccessible(true);
+			method.invoke(mem,-1);
+			//thrown.except(OverflowException.class);
+			//thrown.except(NullPointerException.class);java -cp "./hamcrest-core-1.3.jar:./junit-4.12.jar:./bin" org.junit.runner.JUnitCore test.brainfuck.virtualmachine.MemoryTest
+
+		}catch(Exception e){
+			e.printStackTrace();
+//			e.getTargetException();
+		}
+	}
+*/
+	@Test(expected = NullPointerException.class)
+	public void testExcep(){
+		Memory mem = null;
+		mem.getSize();
 	}
 }
