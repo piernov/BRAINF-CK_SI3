7ca28513209e091d4ed3c6cb677e41683cf6919a
piernov <piernov@piernov.org>
2016-09-29 21:12:49 +0200

    Add TestSuite and IncrTest

---
+++ src/test/brainfuck/virtualmachine/TestSuite.java
@@ -0,0 +1,15 @@
+package test.brainfuck.virtualmachine;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+	IncrTest.class,
+	MemoryTest.class
+})
+
+public class TestSuite {
+  // the class remains empty,
+  // used only as a holder for the above annotations
+}
