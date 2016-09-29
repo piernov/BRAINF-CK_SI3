075e6d828b5ba123397b5ea71fe7cfa4aa8ef6f4
piernov <piernov@piernov.org>
2016-09-29 13:19:55 +0200

    build.sh builds tests

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # build script
 
-javac -d bin/ -cp bin/ `find src/brainfuck -name \*.java`
+javac -d bin/ -cp bin/:/usr/share/java/junit.jar `find src -name \*.java`
 cd bin/
 # See https://docs.oracle.com/javase/tutorial/deployment/jar/appman.html
 jar vcfe ../Bfck.jar brainfuck.Main brainfuck/
