e656d565e59a91da7633559e893a343ab1e2e5dd
piernov <piernov@piernov.org>
2016-09-29 21:12:49 +0200

    Add TestSuite and IncrTest

---
+++ src/test/brainfuck/virtualmachine/IncrTest.java
@@ -0,0 +1,56 @@
+package test.brainfuck.virtualmachine;
+
+import brainfuck.Instruction;
+import brainfuck.instructions.Incr;
+import brainfuck.virtualmachine.Machine;
+import brainfuck.virtualmachine.Memory;
+import brainfuck.virtualmachine.OverflowException;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Field;
+import java.lang.Class;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.Rule;
+
+public class IncrTest{
+	@Test
+	public void incrConstructorAllocation() {
+		Instruction incr = new Incr();
+		assertNotNull(incr);
+	}
+
+	@Test
+	public void incrOne() {
+		Instruction incr = new Incr();
+		Machine machine = new Machine();
+		int val = machine.readMemory();
+		incr.accept(machine);
+		assertEquals(val+1, machine.readMemory());
+	}
+
+	@Test
+	public void incrMax() throws Throwable {
+		Instruction incr = new Incr();
+		Machine machine = new Machine();
+		int value = machine.readMemory();
+		int count = Byte.MAX_VALUE - Byte.MIN_VALUE;
+
+		for (int i = 0; i < count; i++) incr.accept(machine);
+
+		assertEquals(value+count, machine.readMemory());
+	}
+
+	@Test(expected = OverflowException.class)
+	public void incrOverflow() throws Throwable {
+		Instruction incr = new Incr();
+		Machine machine = new Machine();
+		int count = Byte.MAX_VALUE - Byte.MIN_VALUE + 1;
+
+		for (int i = 0; i < count; i++) incr.accept(machine);
+	}
+
+}
--- tests.sh
+++ tests.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-java -cp "./hamcrest-core-1.3.jar:/usr/share/java/junit.jar:./bin" org.junit.runner.JUnitCore test.brainfuck.virtualmachine.MemoryTest
-
+java -cp "./hamcrest-core-1.3.jar:/usr/share/java/junit.jar:./bin" org.junit.runner.JUnitCore test.brainfuck.virtualmachine.TestSuite
