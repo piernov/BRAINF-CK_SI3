6b74af2842b069abcc180bde570b97303ea40313
Tordesillas <bubble83440@gmail.com>
2016-10-08 16:06:06 +0200

    ImageWriter is more flexible and bug fixes

--- src/brainfuck/ImageWriter.java
+++ src/brainfuck/ImageWriter.java
@@ -11,67 +11,82 @@ import java.util.List;
  * Build a bmp image from a list of instructions.
  *
  * @author Miaou
- * @see https://docs.oracle.com/javase/7/docs/api/java/awt/image/BufferedImage.html
+ * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/awt/image/BufferedImage.html>BufferedImage</a>
  * @see Translator
  */
 
 class ImageWriter {
   /**
-   * Size of the image (number of pixels)
-   */
-  private int width;
-  private int height;
-
-  /**
-   * Image represented by a buffer
+   * Image represented by a buffer.
    */
   private BufferedImage image;
 
   /**
-   * Constructs a BufferedImage and writes it
-   *
-   * @param colors    list of the color of each pixel
+   * Instructions to represent in the image.
    */
-  public ImageWriter(List<Integer> colors) {
-    this(colors, 9, 9);
-  }
+  private List<Integer> colors;
 
   /**
-   * Constructs a BufferedImage and writes it
-   *
-   * @param colors    list of the color of each pixel
-   * @param width     width of the image
-   * @param height    height of the image
+   * Width and height of the image (number of pixels).
    */
-  public ImageWriter(List<Integer> colors, int width, int height) {
-    this.width = width;
-    this.height = height;
-    image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-    draw(colors);
+  private int size;
+
+  /**
+   * Size of a square.
+   */
+  private static final int SIZE = 3;
+
+  /**
+   * Number of cases in a column or a line.
+   */
+  private int nbCol;
+
+  /**
+   * Constructs a BufferedImage and writes it.
+   *
+   * @param colors  list of the color of each pixel
+   */
+  public ImageWriter(List<Integer> colors) {
+    this.colors = colors;
+    size = 3*(int)Math.ceil(Math.sqrt(colors.size()));
+    nbCol = size/SIZE;
+    image = new BufferedImage(size, size, BufferedImage.TYPE_INT_RGB);
+    draw();
     writeBmp("./bfck.bmp");
   }
 
-  public void draw(List<Integer> colors) {
-    for (int y = 0; y < width; y+=3) {
-      for (int i = 0; i < 3; i++) {
-        drawLine(colors, y+i, i);
+  /**
+   * Draw each square of the image.
+   */
+  public void draw() {
+    for (int y = 0; y < nbCol; y++) {
+      for (int x = 0; x < nbCol; x++) {
+        drawSquare(x, y);
       }
     }
   }
 
-  public void drawLine(List<Integer> colors, int line, int j) {
-    for (int x = 0; x < width; x+=3) {
-      for (int i = 0; i < 3; i++) {
-        if ((x/3+(line-j)) >= colors.size()) {
-          image.setRGB(x+i, line, 0x000000);
+  /**
+   * Draw each square of the image.
+   */
+  public void drawSquare(int col, int line) {
+    for (int i = 0; i < SIZE; i++) {
+      for (int j = 0; j < SIZE; j++) {
+        if (col+line*nbCol >= colors.size()) {
+          image.setRGB(col*SIZE+i, line*SIZE+j, 0x000000);
         }
         else {
-          image.setRGB(x+i, line, colors.get(x/3+(line-j)));
+          image.setRGB(col*SIZE+i, line*SIZE+j, colors.get(col+line*nbCol));
         }
       }
     }
   }
 
+  /**
+   * Print the image.
+   *
+   * @param path  path of the destination of the image
+   */
   public void writeBmp(String path) {
     try {
       RenderedImage rendImage = image;
