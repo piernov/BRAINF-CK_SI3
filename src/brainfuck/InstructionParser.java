4d226f02b0b931c948bd951309433b929863484e
piernov <piernov@piernov.org>
2016-11-03 19:10:18 +0100

    InstructionParser improvements on text

--- src/brainfuck/InstructionParser.java
+++ src/brainfuck/InstructionParser.java
@@ -44,29 +44,27 @@ public class InstructionParser {
 		this();
 
 		// Does it need a .filter(l -> !"".equals) ?
-		stream.forEachOrdered(line -> {
-			if (!line.startsWith("#")) {
-				if (line.contains("#")) {
-					line = line.substring(0, line.indexOf("#"));
-				}
-				Instruction instr = iset.getOp((String)line.trim()); // Tries to parse the whole line (ie. long format)
-				if (instr != null) {
+		stream.filter(l -> !l.startsWith("#"))
+		.map(l -> {
+			int p = l.indexOf("#");
+			if (p > 0) l = l.substring(0, p);
+			return l.trim();
+		})
+		.forEachOrdered(line -> {
+			Instruction instr = iset.getOp(line); // Tries to parse the whole line (ie. long format)
+			if (instr != null) {
+				instructions.add(instr);
+			} else {
+				for (int i = 0; i < line.length(); i++) { // Tries to executes the instructions with the short format
+					char c = line.charAt(i);
+					if (c == ' ' || c == '\t') continue;
+
+					instr = iset.getOp(c);
+					if (instr == null) throw new InvalidInstructionException(c);
 					instructions.add(instr);
-				} else {
-					for (int i = 0; i < line.length(); i++) { // Tries to executes the instructions with the short format
-						char c = line.charAt(i);
-						if (c != ' ' && c != '\t') {
-							instr = iset.getOp(c);
-							if (instr != null) instructions.add(instr);
-							else {
-								throw new InvalidInstructionException(c);
-							}
-						}
-					}
 				}
 			}
 		});
-
 	}
 
 	/**
